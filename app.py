7446a06fc1c78435ea867ad99d88f278e9600300
amhvost <119269949+amhvpste@users.noreply.github.com>
2025-09-27 21:42:14 +0300

    test fix

---
+++ app.py
@@ -0,0 +1,17 @@
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Привіт, це Tornado!")
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    print("Сервер запущено на http://localhost:8888")
+    tornado.ioloop.IOLoop.current().start()
